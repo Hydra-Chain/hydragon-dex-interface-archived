7c0918c6f05bf2c86e6cb647fa8e07e20a069209
Samuil Borisov <88675952+SamBorisov@users.noreply.github.com>
2024-12-23 16:32:37 +0200

    Hydragon dex polishing (#2)
    * generate graph files
    
    * remove unused files to build properly
    
    * remove setFiatOnrampAvailability
    
    * comment moonpay stuff
    
    * add telegram url for get support link and console log
    
    * remove onramp announcement and remove unsupported assets section
    
    * add privacy policy url and remove some redundant stuff
    
    rebrand Uniswap -> HydraDex and use Hydra Chain where applicable;
    change weth -> whydra;
    add new env variables for links used for hydra;
    
    * comment discordIcon to avoid build err
    
    * new urls for the hydra dex and the info page
    
    * add new env variables and support for different chains
    
    * update on the fiat value and other apis
    
    remove the fiat value fetching and the amplitude api;
    change the contract addresses with the newly deployed ones;
    add env variables and support for devnet;
    increase precision for the balance formatting;
    
    * disable chain selector if 1 chain
    
    * update coinbase link
    
    * change styling on glow & add more theme changes
    
    * Fix Polish readme.md and contribution.md
    
    * update image folder
    
    * fill the about page and
    
    * update comments for important things to SAMVI
    
    ---------
    
    Co-authored-by: Vitomir Pavlov <vitomirpavlov@gmail.com>

--- src/components/About/constants.tsx
+++ src/components/About/constants.tsx
@@ -1,34 +1,46 @@
 import { InterfaceElementName } from '@uniswap/analytics-events'
+import { HYDRACHAIN_DEVELOPERS_URL, HYDRACHAIN_DEX_INFO_URL } from 'constants/chainInfo'
 import { DollarSign, Terminal } from 'react-feather'
 import styled from 'styled-components/macro'
 import { lightTheme } from 'theme/colors'
 
+import hydraDragon from '../../assets/hydra/hydra-guard.png'
+import hydraDragonLight from '../../assets/hydra/hydra-guard-whiteTheme.png'
 import darkArrowImgSrc from './images/aboutArrowDark.png'
 import lightArrowImgSrc from './images/aboutArrowLight.png'
 import darkDollarImgSrc from './images/aboutDollarDark.png'
 import darkTerminalImgSrc from './images/aboutTerminalDark.png'
-import nftCardImgSrc from './images/nftCard.png'
 import swapCardImgSrc from './images/swapCard.png'
 
 export const MAIN_CARDS = [
   {
+    to: '/add',
+    title: 'Create pools',
+    description: 'Create pools with your unique token pair on HydraDex and earn fees on swaps.',
+    cta: 'Create a pool',
+    darkBackgroundImgSrc: hydraDragon,
+    lightBackgroundImgSrc: hydraDragonLight,
+    elementName: InterfaceElementName.ABOUT_PAGE_NFTS_CARD,
+  },
+  {
     to: '/swap',
     title: 'Swap tokens',
-    description: 'Buy, sell, and explore tokens on Ethereum, Polygon, Optimism, and more.',
+    description: 'Buy, sell, and explore tokens on Hydra Chain.',
     cta: 'Trade Tokens',
     darkBackgroundImgSrc: swapCardImgSrc,
     lightBackgroundImgSrc: swapCardImgSrc,
     elementName: InterfaceElementName.ABOUT_PAGE_SWAP_CARD,
   },
-  {
-    to: '/nfts',
-    title: 'Trade NFTs',
-    description: 'Buy and sell NFTs across marketplaces to find more listings at better prices.',
-    cta: 'Explore NFTs',
-    darkBackgroundImgSrc: nftCardImgSrc,
-    lightBackgroundImgSrc: nftCardImgSrc,
-    elementName: InterfaceElementName.ABOUT_PAGE_NFTS_CARD,
-  },
+  // SAMVI Unused: We currently do not have the NFTs option
+  // {
+  //   to: '/nfts',
+  //   title: 'Trade NFTs',
+  //   description: 'Buy and sell NFTs across marketplaces to find more listings at better prices.',
+  //   cta: 'Explore NFTs',
+  //   darkBackgroundImgSrc: nftCardImgSrc,
+  //   lightBackgroundImgSrc: nftCardImgSrc,
+  //   elementName: InterfaceElementName.ABOUT_PAGE_NFTS_CARD,
+  // },
 ]
 
 const StyledCardLogo = styled.img`
@@ -39,30 +51,41 @@ const StyledCardLogo = styled.img`
 `
 
 export const MORE_CARDS = [
-  {
-    to: 'https://support.uniswap.org/hc/en-us/articles/11306574799117-How-to-use-Moon-Pay-on-the-Uniswap-web-app-',
-    external: true,
-    title: 'Buy crypto',
-    description: 'Buy crypto with your credit card or bank account at the best rates.',
-    lightIcon: <DollarSign color={lightTheme.textTertiary} size={48} />,
-    darkIcon: <StyledCardLogo src={darkDollarImgSrc} alt="Earn" />,
-    cta: 'Buy now',
-    elementName: InterfaceElementName.ABOUT_PAGE_BUY_CRYPTO_CARD,
-  },
+  // SAMVI Unused: We currently do not have the buy option
+  // {
+  //   to: 'https://support.uniswap.org/hc/en-us/articles/11306574799117-How-to-use-Moon-Pay-on-the-Uniswap-web-app-',
+  //   external: true,
+  //   title: 'Buy crypto',
+  //   description: 'Buy crypto with your credit card or bank account at the best rates.',
+  //   lightIcon: <DollarSign color={lightTheme.textTertiary} size={48} />,
+  //   darkIcon: <StyledCardLogo src={darkDollarImgSrc} alt="Earn" />,
+  //   cta: 'Buy now',
+  //   elementName: InterfaceElementName.ABOUT_PAGE_BUY_CRYPTO_CARD,
+  // },
   {
     to: '/pool',
     title: 'Earn',
-    description: 'Provide liquidity to pools on Uniswap and earn fees on swaps.',
-    lightIcon: <StyledCardLogo src={lightArrowImgSrc} alt="Analytics" />,
-    darkIcon: <StyledCardLogo src={darkArrowImgSrc} alt="Analytics" />,
+    description: 'Provide liquidity to an existing pools on HydraDex and earn fees on swaps.',
+    lightIcon: <StyledCardLogo src={lightArrowImgSrc} alt="Earn" />,
+    darkIcon: <StyledCardLogo src={darkArrowImgSrc} alt="Earn" />,
     cta: 'Provide liquidity',
     elementName: InterfaceElementName.ABOUT_PAGE_EARN_CARD,
   },
   {
-    to: 'https://docs.uniswap.org',
+    to: HYDRACHAIN_DEX_INFO_URL,
+    external: true,
+    title: 'Track pricing',
+    description: 'Track tokens prices, pools volumes and statistics for on HydraDex.',
+    lightIcon: <DollarSign color={lightTheme.textTertiary} size={48} />,
+    darkIcon: <StyledCardLogo src={darkDollarImgSrc} alt="Statistics" />,
+    cta: 'Swap statistics',
+    elementName: InterfaceElementName.ABOUT_PAGE_EARN_CARD,
+  },
+  {
+    to: HYDRACHAIN_DEVELOPERS_URL,
     external: true,
     title: 'Build dApps',
-    description: 'Build apps and tools on the largest DeFi protocol on Ethereum.',
+    description: 'Build apps and tools on the largest DeFi protocol on Hydra Chain.',
     lightIcon: <Terminal color={lightTheme.textTertiary} size={48} />,
     darkIcon: <StyledCardLogo src={darkTerminalImgSrc} alt="Developers" />,
     cta: 'Developer docs',
